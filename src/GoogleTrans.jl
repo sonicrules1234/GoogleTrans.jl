1c8e42b4d7880a2de0439b8d5c8f9be1c2e0948b
Jarrod Urban <jarrod@jarrod.de>
2021-11-27 09:45:18 +0100

    a quick fixup

--- src/GoogleTrans.jl
+++ src/GoogleTrans.jl
@@ -2,7 +2,6 @@ module GoogleTrans
 import HTTP
 import JSON
 export translate
-#export batchtranslate
 
 function package_rpc(text::AbstractString, lang_src::String, lang_tgt::String)::String
     parameter = [[text, lang_src, lang_tgt, true], [1]]
@@ -13,33 +12,13 @@ function package_rpc(text::AbstractString, lang_src::String, lang_tgt::String)::
     return freq
 end
 
-function translate(text::AbstractString, lang_tgt="auto"::String, lang_src="auto"::String):String
-    headers = Dict("Referer"=>"http://translate.google.com", "User-Agent"=>"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36", "Content-type"=>"application/x-www-form-urlencoded;charset=utf-8")
+function translate(text::AbstractString, lang_tgt = "auto"::String, lang_src = "auto"::String)::String
+    headers = Dict("Referer" => "http://translate.google.com", "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36", "Content-type" => "application/x-www-form-urlencoded;charset=utf-8")
     freq = package_rpc(text, lang_src, lang_tgt)
     r = HTTP.request("POST", "https://translate.google.com/_/TranslateWebserverUi/data/batchexecute", headers, freq)
-    resp = String(r.body)
-    for line in split(resp, "\n")
-        if occursin("MkEWBc", line)
-            response = line * "]"
-            parsed = JSON.parse(response)
-            #println(parsed)
-            goodparsed = JSON.parse(parsed[1][3])[2][1][1][6]
-            translated = ""
-            for sentencelist in goodparsed
-                translated = translated * " " * sentencelist[1] 
-            end
-            return translated[2:end]
-        end
-    end
+    JSON.parse(JSON.parse(split(String(r.body), '\n')[end])[1][3])[2][1][1][6][1][1]
 end
 
-translate(inputstrings::AbstractVector{S}, lang_tgt="auto", lang_src="auto") where {S <: AbstractString} = translate.(inputstrings, lang_tgt, lang_src)
+translate(inputstrings::AbstractVector{S}, lang_tgt = "auto", lang_src = "auto") where {S<:AbstractString} = translate.(inputstrings, lang_tgt, lang_src)
 
-#function batchtranslate(inputstrings::Vector{AbstractString}, lang_tgt="auto"::String, lang_src="auto"::String)::Vector{String}
-#    returnparts = Vector{String}()
-#    for part in inputstrings
-#        append!(returnparts, translate(part))
-#    end
-#    return returnparts
-#end
 end
